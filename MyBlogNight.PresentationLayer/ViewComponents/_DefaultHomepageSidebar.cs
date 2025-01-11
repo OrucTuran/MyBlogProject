a3ccc87bf9a5e9b63648e834da1386d57ca33c80
oructuran0 <orctrn193@gmail.com>
2025-01-12 01:22:56 +0300

    Home Page Sidebar Latest Posts have been made dynamic and the last 3 added are listed

---
+++ MyBlogNight.PresentationLayer/ViewComponents/_DefaultHomepageSidebar.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MyBlogNight.BusinessLayer.Abstract;
+
+namespace MyBlogNight.PresentationLayer.ViewComponents
+{
+    public class _DefaultHomepageSidebar : ViewComponent
+    {
+        private readonly IArticleService _articleService;
+
+        public _DefaultHomepageSidebar(IArticleService articleService)
+        {
+            _articleService = articleService;
+        }
+        public IViewComponentResult Invoke()
+        {
+            var values = _articleService.TArticleListWithCategoryAndAppUser()
+                .OrderByDescending(a => a.CreatedDate)
+                            .Take(3)
+                            .ToList();
+            return View(values);
+        }
+    }
+}
