e1ae4ca797f4360821f814fb4054248ac39ab3c1
oructuran0 <orctrn193@gmail.com>
2025-01-12 01:27:32 +0300

    Home page footer popular categories sorted by views

---
+++ MyBlogNight.PresentationLayer/ViewComponents/_DefaultHomepageFooterPopulerCategories.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MyBlogNight.BusinessLayer.Abstract;
+
+namespace MyBlogNight.PresentationLayer.ViewComponents
+{
+    public class _DefaultHomepageFooterPopulerCategories : ViewComponent
+    {
+        private readonly IArticleService _articleService;
+
+        public _DefaultHomepageFooterPopulerCategories(IArticleService articleService)
+        {
+            _articleService = articleService;
+        }
+        public IViewComponentResult Invoke()
+        {
+            var categoriesWithArticleCount = _articleService.TGetCategoriesWithArticleCount().Take(7).ToList();
+            return View(categoriesWithArticleCount);
+        }
+    }
+}
