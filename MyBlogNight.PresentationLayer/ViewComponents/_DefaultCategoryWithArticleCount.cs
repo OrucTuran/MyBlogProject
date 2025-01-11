44756f4a62d7b15327726d683f1240863569ad66
oructuran0 <orctrn193@gmail.com>
2025-01-12 01:21:54 +0300

    Home page footer popular categories sorted by views

---
+++ MyBlogNight.PresentationLayer/ViewComponents/_DefaultCategoryWithArticleCount.cs
@@ -0,0 +1,21 @@
+﻿
+using Microsoft.AspNetCore.Mvc;
+using MyBlogNight.BusinessLayer.Abstract;
+
+namespace MyBlogNight.PresentationLayer.ViewComponents
+{
+    public class _DefaultCategoryWithArticleCount : ViewComponent
+    {
+        private readonly IArticleService _articleService;
+
+        public _DefaultCategoryWithArticleCount(IArticleService articleService)
+        {
+            _articleService = articleService;
+        }
+        public IViewComponentResult Invoke()
+        {
+            var categoriesWithArticleCount = _articleService.TGetCategoriesWithArticleCount();
+            return View(categoriesWithArticleCount);
+        }
+    }
+}
