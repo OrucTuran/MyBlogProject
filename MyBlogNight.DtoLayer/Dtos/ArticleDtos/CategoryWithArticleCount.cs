454f2492916c045df94d590cb83c9b7510a27611
oructuran0 <orctrn193@gmail.com>
2025-01-12 01:20:54 +0300

    Home page footer popular categories sorted by views

---
+++ MyBlogNight.DtoLayer/Dtos/ArticleDtos/CategoryWithArticleCount.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyBlogNight.DtoLayer.Dtos.ArticleDtos
+{
+    public class CategoryWithArticleCount
+    {
+        public string CategoryName { get; set; }
+        public int ArticleCount { get; set; }
+    }
+}
