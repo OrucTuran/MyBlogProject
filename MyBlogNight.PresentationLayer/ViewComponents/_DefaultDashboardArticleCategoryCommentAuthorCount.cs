b0439a74f8fba175a304731221c8f1f454250de4
oructuran0 <orctrn193@gmail.com>
2025-02-02 23:16:01 +0300

    Dashboard ArticleCategoryCommentAuthorCount

---
+++ MyBlogNight.PresentationLayer/ViewComponents/_DefaultDashboardArticleCategoryCommentAuthorCount.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MyBlogNight.BusinessLayer.Abstract;
+using MyBlogNight.DtoLayer.Dtos.DashboardDtos;
+
+namespace MyBlogNight.PresentationLayer.ViewComponents
+{
+    public class _DefaultDashboardArticleCategoryCommentAuthorCount : ViewComponent
+    {
+        private readonly IArticleService _articleService;
+        private readonly ICategoryService _categoryService;
+        private readonly IAppUserService _appUserService;
+        private readonly ICommentService _commentService;
+
+        public _DefaultDashboardArticleCategoryCommentAuthorCount(
+            IArticleService articleService,
+            ICategoryService categoryService,
+            IAppUserService appUserService,
+            ICommentService commentService)
+        {
+            _articleService = articleService;
+            _categoryService = categoryService;
+            _appUserService = appUserService;
+            _commentService = commentService;
+        }
+        public IViewComponentResult Invoke()
+        {
+            var dto = new ArticleCategoryCommentAuthorCountDTO
+            {
+                TotalArticleCount = _articleService.TGetAll().Count(),
+                TotalCategoryCount = _categoryService.TGetAll().Count(),
+                TotalCommentCount = _commentService.TGetAll().Count(),
+                TotalAuthorCount = _appUserService.TGetAll().Count(x => x.isAuthor)
+            };
+            return View(dto);
+        }
+    }
+}
