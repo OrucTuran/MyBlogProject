ef448cdec7f1a22c79336879d084c292caf9c047
oructuran0 <orctrn193@gmail.com>
2025-02-02 23:18:25 +0300

    Dashboard Selective Comments

---
+++ MyBlogNight.PresentationLayer/ViewComponents/_DefaultDashboardSelectiveComments.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MyBlogNight.BusinessLayer.Abstract;
+
+namespace MyBlogNight.PresentationLayer.ViewComponents
+{
+    public class _DefaultDashboardSelectiveComments : ViewComponent
+    {
+        private readonly ICommentService _commentService;
+
+        public _DefaultDashboardSelectiveComments(ICommentService commentService)
+        {
+            _commentService = commentService;
+        }
+        public IViewComponentResult Invoke()
+        {
+            var values = _commentService.TGetAll().Take(5).ToList();
+            return View(values);
+        }
+    }
+}
