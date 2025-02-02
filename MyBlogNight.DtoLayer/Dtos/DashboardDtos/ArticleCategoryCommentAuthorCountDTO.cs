fa8d91ce672052bec472db3b7bc9be59dae76242
oructuran0 <orctrn193@gmail.com>
2025-02-02 23:17:50 +0300

    ArticleCategoryCommentAuthorCount DTO

---
+++ MyBlogNight.DtoLayer/Dtos/DashboardDtos/ArticleCategoryCommentAuthorCountDTO.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyBlogNight.DtoLayer.Dtos.DashboardDtos
+{
+    public class ArticleCategoryCommentAuthorCountDTO
+    {
+        public int TotalArticleCount { get; set; }
+        public int TotalCategoryCount { get; set; }
+        public int TotalCommentCount { get; set; }
+        public int TotalAuthorCount { get; set; }
+    }
+}
