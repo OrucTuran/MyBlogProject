aa0230918c0cd50130299487519316dc3468f32c
oructuran0 <orctrn193@gmail.com>
2025-02-03 23:58:20 +0300

    Dashboard populer users listing

---
+++ MyBlogNight.PresentationLayer/ViewComponents/_DefaultDashboardPopulerMembers.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MyBlogNight.BusinessLayer.Abstract;
+
+namespace MyBlogNight.PresentationLayer.ViewComponents
+{
+    public class _DefaultDashboardPopulerMembers : ViewComponent
+    {
+        private readonly ICommentService _commentService;
+
+        public _DefaultDashboardPopulerMembers(ICommentService commentService)
+        {
+            _commentService = commentService;
+        }
+        public IViewComponentResult Invoke()
+        {
+            var activeUsers = _commentService.TGetMostActiveUsers();
+            return View(activeUsers);
+        }
+    }
+}
